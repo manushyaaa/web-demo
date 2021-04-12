881e8f75ff59687c8efb26e57bcc898b664eb3ba
Manu Sunil <46190721+manushyaaa@users.noreply.github.com>
2021-04-12 23:45:51 +0530

    Update start.py

--- start.py
+++ start.py
@@ -3,6 +3,14 @@ import mysql.connector
 app = Flask(__name__)
 
 
+mydb = mysql.connector.connect(
+   host="localhost",
+   user="root",
+   password="",
+   database="webdemo"
+   )
+mycursor = mydb.cursor()
+
 @app.route('/')
 def hello_world():
     return render_template("index.html")
@@ -11,26 +19,14 @@ def hello_world():
    
 @app.route('/omen')
 def exam_resources():
-   mydb = mysql.connector.connect(
-   host="localhost",
-   user="root",
-   password="",
-   database="webdemo"
-   )
-   mycursor = mydb.cursor()
+  
    mycursor.execute("SELECT * FROM omen")
    myresult = mycursor.fetchall()
    return render_template("index2.html", myresult=myresult)
 
 @app.route("/sova")
 def  bible_resources():
-   mydb = mysql.connector.connect(
-   host="localhost",
-   user="root",
-   password="",
-   database="webdemo"
-   )
-   mycursor = mydb.cursor()
+   
    mycursor.execute("SELECT * FROM sova")
    myresult = mycursor.fetchall()
    return render_template("index3.html", myresult=myresult)
